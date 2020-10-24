f16ae32dc9a26a8f2810966c22a053a0ea50844b
Greg Marsh <gregmarsh@Gregs-iMac.local>
2020-10-23 17:18:52 -0700

    joins exercise

---
+++ queries.sql
@@ -0,0 +1,18 @@
+SELECT *
+ FROM owners
+ FULL JOIN vehicles
+ ON owners.id = vehicles.owner_id;
+
+ SELECT first_name, last_name, COUNT(*)
+ FROM owners
+ JOIN vehicles
+ ON owners.id = vehicles.owner_id
+ GROUP BY owners.id
+ ORDER BY count desc;
+
+SELECT first_name, last_name, ROUND(AVG(price)) AS average_price, COUNT(owner_id)
+ FROM owners o
+ JOIN vehicles v ON o.id=v.owner_id
+ GROUP BY (first_name, last_name)
+ HAVING COUNT(owner_id) > 1 AND ROUND(AVG(price)) > 10000
+ ORDER BY first_name desc;
